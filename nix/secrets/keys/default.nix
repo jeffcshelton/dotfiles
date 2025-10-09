1780df706f17a212f12b725d275fc002605723cc
Jeff Shelton <jeff@shelton.one>
2025-10-09 00:39:23 -0400

    Add all public keys

---
+++ nix/secrets/keys/default.nix
@@ -0,0 +1,36 @@
+let
+  # Define custom `removeSuffix` function to avoid requiring lib.
+  removeSuffix = suffix: string: (
+    builtins.substring
+      0
+      (builtins.stringLength string - builtins.stringLength suffix)
+      string
+  );
+
+  # Read the current directory and treat the directories as host listings.
+  entries = builtins.readDir ./.;
+  hosts = builtins.filter
+    (file: entries."${file}" == "directory")
+    (builtins.attrNames entries);
+in
+# Construct an object of the form { "<host>": { "<user": "<key>", ... }, ... }.
+# NOTE: The host directories must only contain ".pub" files.
+builtins.listToAttrs (map
+  (host: {
+    name = host;
+    value =
+      let
+        users = builtins.filter
+          (file: (builtins.match ".*\\.pub$" file) != null)
+          (builtins.attrNames (builtins.readDir ./${host}));
+      in
+      builtins.listToAttrs (map
+        (file: {
+          name = removeSuffix ".pub" file;
+          value = removeSuffix "\n" (builtins.readFile ./${host}/${file});
+        })
+        users
+      );
+  })
+  hosts
+)
