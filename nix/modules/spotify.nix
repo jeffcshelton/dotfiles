226f7dd20332cfd5f79f422ce19d31d1d3e7a11b
Jeff Shelton <jeff@shelton.one>
2026-01-22 15:06:00 -0500

    Remove pinned Spotify package

--- nix/modules/spotify.nix
+++ nix/modules/spotify.nix
@@ -1,17 +1,6 @@
 { pkgs, ... }:
-let
-  # FIXME: Temporary fix for #465676. Remove when fixed.
-  spotify = pkgs.spotify.overrideAttrs (old: {
-    src = if (pkgs.stdenv.isDarwin && pkgs.stdenv.isAarch64)
-      then pkgs.fetchurl {
-        url = "https://web.archive.org/web/20251029235406/https://download.scdn.co/SpotifyARM64.dmg";
-        hash = "sha256-0gwoptqLBJBM0qJQ+dGAZdCD6WXzDJEs0BfOxz7f2nQ=";
-      }
-      else old.src;
-  });
-in
 {
-  environment.systemPackages = [
+  environment.systemPackages = with pkgs; [
     spotify
   ];
 }
